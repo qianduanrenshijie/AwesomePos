e843276e03df4d638ba023a12c2cce45c5531714
renshijie <1406836953@qq.com>
2019-03-06 23:55:26 +0800

    creater icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <i class="iconfont icon-hanbao"></i>
   </div>
 </template>
 
